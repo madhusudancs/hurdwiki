2e315ef11db39e8ee0a0decc054801521ee049dd
Samuel Thibault <samuel.thibault@ens-lyon.org>
2011-11-05 15:23:57 +0100

    better formatting

--- open_issues/performance/io_system/test-lseek.c
+++ open_issues/performance/io_system/test-lseek.c
@@ -3,14 +3,15 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/time.h>
+#define N 100000
 int main(void) {
 	int fd = open("test.c", O_RDONLY);
 	struct timeval tv1, tv2;
 	int i;
 	gettimeofday(&tv1, NULL);
-	for (i = 0; i < 100000; i++)
+	for (i = 0; i < N; i++)
 		lseek(fd, 0, SEEK_CUR);
 	gettimeofday(&tv2, NULL);
-	printf("%07lu\n", (tv2.tv_sec-tv1.tv_sec) * 1000000 + tv2.tv_usec - tv1.tv_usec);
+	printf("%fµs\n", (float)((tv2.tv_sec-tv1.tv_sec) * 1000000 + tv2.tv_usec - tv1.tv_usec)/N);
 	return 0;
 }
--- open_issues/performance/io_system/test-mach.c
+++ open_issues/performance/io_system/test-mach.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <mach/mach.h>
+#define N 1000000
 int main(void) {
 	struct timeval tv1, tv2;
 	int i;
@@ -9,10 +10,10 @@ int main(void) {
 	task = mach_task_self();
 	mach_port_urefs_t refs;
 	gettimeofday(&tv1, NULL);
-	for (i = 0; i < 1000000; i++) {
+	for (i = 0; i < N; i++) {
 		mach_port_get_refs(task, task, MACH_PORT_RIGHT_RECEIVE, &refs);
 	}
 	gettimeofday(&tv2, NULL);
-	printf("%07lu\n", (tv2.tv_sec-tv1.tv_sec) * 1000000 + tv2.tv_usec - tv1.tv_usec);
+	printf("%fµs\n", (float)((tv2.tv_sec-tv1.tv_sec) * 1000000 + tv2.tv_usec - tv1.tv_usec)/N);
 	return 0;
 }
